ba330a11ab6e0128c21ee294e0466e18d8379a5a
jedid <jedidiah184@gmail.com>
2026-02-07 13:30:28 -0700

    Organizing site pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('projects/', views.projects, name='projects'),
 ]
